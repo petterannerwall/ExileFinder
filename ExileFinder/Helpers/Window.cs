1fbafa32f25f0d2af07fd69dfd3632e6cc81b344
Petter Annerwall <petterannerwall@gmail.com>
2016-11-17 21:50:40 +0100

    Added winhooks
    Temporary connected to notepad

---
+++ ExileFinder/Helpers/Window.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExileFinder.Helpers
+{
+    public class Window
+    {
+        public bool Valid { get; protected set; }
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public Window()
+        {
+            var dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+        #region GetActiveWindow code
+        private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+
+
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            string activeWindow = GetActiveWindowTitle();
+
+            if (activeWindow != null)
+            {
+                if (!activeWindow.Contains("ExileFinder") && !activeWindow.Contains("Notepad") && !activeWindow.Contains("menu"))
+                {
+                    this.Valid = false;
+                }
+                if (activeWindow.Contains("ExileFinder") || activeWindow.Contains("Notepad"))
+                {
+                    this.Valid = true;
+                }
+            }
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
--- ExileFinder/PageSwitcher.xaml.cs
+++ ExileFinder/PageSwitcher.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,14 +20,21 @@ namespace ExileFinder
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class PageSwitcher : Window
+    public partial class PageSwitcher : Window, ISwitchable
     {
+
+        private WinEventDelegate dele;
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
 
         public PageSwitcher()
         {
             InitializeComponent();
             Switcher.PageSwitcher = this;
             Switcher.Switch(new Start());
+
+            dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
         }
 
         public void Navigate(UserControl nextPage)
@@ -46,12 +54,65 @@ namespace ExileFinder
                   + nextPage.Name.ToString());
         }
 
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
-
-            // Begin dragging the window
             this.DragMove();
         }
+
+        #region GetActiveWindow code
+
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            string activeWindow = GetActiveWindowTitle();
+
+            if (activeWindow != null)
+            {
+                if (!activeWindow.Contains("Notepad") && !activeWindow.Contains("Exile") && !activeWindow.Contains("menu"))
+                {
+                    this.WindowState = WindowState.Minimized;
+                    this.Topmost = false;
+                }
+                if (activeWindow.Contains("Notepad") || activeWindow.Contains("Exile"))
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Topmost = true;
+                }
+            }
+        }
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+        
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+        
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+        #endregion
+
+
+
     }
 }
